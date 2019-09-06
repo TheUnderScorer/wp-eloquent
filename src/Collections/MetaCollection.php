01aa1965c6da2b838a91be43e9b6eb0cd237e735
Przemysław Żydek <przemyslawzydek@gmail.com>
2019-09-06 18:32:20 +0200

    Changed invalid parent

--- src/Collections/MetaCollection.php
+++ src/Collections/MetaCollection.php
@@ -2,30 +2,39 @@
 
 namespace UnderScorer\ORM\Collections;
 
-use Illuminate\Support\Collection;
+use Illuminate\Database\Eloquent\Collection;
 
+/**
+ * Class MetaCollection
+ * @package UnderScorer\ORM\Collections
+ */
 class MetaCollection extends Collection
 {
     /**
-     * @param string $key
-     * @return mixed
-     */
-    public function __get($key)
-    {
-        if (isset($this->items) && count($this->items)) {
-            $meta = $this->first(function ($meta) use ($key) {
-                return $meta->meta_key === $key;
-            });
-            return $meta ? $meta->meta_value : null;
-        }
-        return null;
-    }
-    /**
      * @param string $name
+     *
      * @return bool
      */
-    public function __isset($name)
+    public function __isset( $name )
     {
-        return !is_null($this->__get($name));
+        return ! is_null( $this->__get( $name ) );
+    }
+
+    /**
+     * @param string $key
+     *
+     * @return mixed
+     */
+    public function __get( $key )
+    {
+        if ( isset( $this->items ) && count( $this->items ) ) {
+            $meta = $this->first( function ( $meta ) use ( $key ) {
+                return $meta->meta_key === $key;
+            } );
+
+            return $meta ? $meta->meta_value : null;
+        }
+
+        return null;
     }
 }
